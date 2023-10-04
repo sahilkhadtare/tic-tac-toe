d91b07519506a9e58ddaa27c8eb89d47cd10689f
sahilkhadtare <sahil.khadtare@objectedge.com>
2023-10-04 21:48:22 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -1,109 +1,123 @@
-import './App.css';
-import {useContext, useState} from 'react'
-import ThemeContext from './Context/ThemeContext'
-import authorImg from './dsa.jpg'
+import "./App.css";
+import { useContext, useEffect, useState } from "react";
+import ThemeContext from "./Context/ThemeContext";
+import authorImg from "./dsa.jpg";
 
+const box = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
-function Square ({value1, onSquareClick}){
-  return(    
-    <button className='square' onClick={onSquareClick}>{value1}</button>
+function Square({ value1, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value1}
+    </button>
   );
 }
 
 function App() {
-  
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   const theme = useContext(ThemeContext);
   const [Theme, setTheme] = useState(theme);
+  const [status, setStatus] = useState("");
 
-  const themeHandler = ()=>{
-    setTheme(Theme==='black'?'white':'black');
+  const themeHandler = () => {
+    setTheme(Theme === "black" ? "white" : "black");
     console.log(Theme);
-  }
+  };
 
-  const onSquareClick = (i)=>{
-    if(squares[i]|| winner(squares)){
+  const onSquareClick = (i) => {
+    if (squares[i] || winner(squares)) {
       return;
     }
     const newArr = squares.slice();
-    if(xIsNext){
-      newArr[i] = 'X';
-    }else{
-      newArr[i] = 'O';
+    if (xIsNext) {
+      newArr[i] = "X";
+    } else {
+      newArr[i] = "O";
     }
     setSquares(newArr);
     setXIsNext(!xIsNext);
   };
 
-
-const winner =(squares)=>{
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for(let i=0;i<lines.length ; i++)
-  {
-    const [a, b, c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-      return squares[a];
+  const winner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    for (const element of lines) {
+      const [a, b, c] = element;
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
     }
-  }
-  return null;
-};
+    return null;
+  };
 
+  useEffect(() => {
+    const win = winner(squares);
 
-const win = winner(squares);
-let status;
-if(win){
-  status = " Winner : " + win;
+    if (win) {
+      setStatus(" Winner : " + win);
+    } else {
+      setStatus("Next player : " + (xIsNext ? "X" : "O"));
+    }
+  }, [squares, xIsNext]);
+
   // alert("Winner : "+ win);
-//   status = `<dialog >
-//   <p>Greetings, one and all!</p>
-//   <form method="dialog">
-//     <button>OK</button>
-//   </form>
-// </dialog>`
-}else{
-  status = "Next player : " + (xIsNext? "X":"O");
-}
+  //   status = `<dialog >
+  //   <p>Greetings, one and all!</p>
+  //   <form method="dialog">
+  //     <button>OK</button>
+  //   </form>
+  // </dialog>`
+
   return (
-<ThemeContext.Provider value={theme}>
-    <div className="App" id='app' style={{color:`${Theme}`}}>
-      <button className='theme-btn' onClick={themeHandler}>ChangeTheme</button>
-      <div className="img" id='author'>
-      <img src={authorImg} alt="Author" />
-       </div>
-      <header className="App-header">
-       <h1>Tic Tac Toe</h1>
-       {status}
-      </header>
-      <div className="gameboard">
-        <div className="square-row"> 
-            <Square value1={squares[0]} onSquareClick={()=>onSquareClick(0)}/>
-            <Square value1={squares[1]} onSquareClick={()=>onSquareClick(1)}/>
-            <Square value1={squares[2]} onSquareClick={()=>onSquareClick(2)}/>
+    <ThemeContext.Provider value={theme}>
+      <div className="App" id="app" style={{ color: `${Theme}` }}>
+        <button className="theme-btn" onClick={themeHandler}>
+          ChangeTheme
+        </button>
+        <div className="img" id="author">
+          <img src={authorImg} alt="Author" />
         </div>
-        <div className="square-row">
-            <Square value1={squares[3]} onSquareClick={()=>onSquareClick(3)}/>
-            <Square value1={squares[4]} onSquareClick={()=>onSquareClick(4)}/>
-            <Square value1={squares[5]} onSquareClick={()=>onSquareClick(5)}/>
-        </div>
-        <div className="square-row">
-            <Square value1={squares[6]} onSquareClick={()=>onSquareClick(6)}/>
-            <Square value1={squares[7]} onSquareClick={()=>onSquareClick(7)}/>
-            <Square value1={squares[8]} onSquareClick={()=>onSquareClick(8)}/>
+        <header className="App-header">
+          <h1>Tic Tac Toe</h1>
+          {status}
+        </header>
+        <div className="gameboard">
+          {box.map((row, index) => {
+            return (
+              <div key={index} className="square-row">
+                {row.map((value) => {
+                  return (
+                    <Square
+                      key={value}
+                      value1={squares[value]}
+                      onSquareClick={() => onSquareClick(value)}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
         </div>
       </div>
-    </div>
-    
-</ThemeContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
